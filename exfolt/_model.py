17e5b6e1d1c18fcb03ce2084f20216b85fbdce83
eXhumer <exhumer@exhumer.cc>
2022-04-22 00:28:19 -0600

    feat: Implement repr function for model classes

--- exfolt/_model.py
+++ exfolt/_model.py
@@ -30,7 +30,10 @@ class Snowflake:
         return str(self.__value)
 
     def __repr__(self) -> str:
-        return "Snowflake(value={self.__value}, )"
+        return f"Snowflake(value={self.__value}, timestamp={self.timestamp}" +\
+            f", internal_process_id={self.internal_process_id}, " +\
+            f"internal_worker_id={self.internal_worker_id}, " +\
+            f"increment={self.increment})"
 
     @property
     def __discord_epoch_timestamp_ms(self):
@@ -71,6 +74,11 @@ class DiscordModel:
             self.users = users
             self.replied_user = replied_user
 
+        def __repr__(self) -> str:
+            return f"DiscordModel.AllowedMention(parse={self.parse}, " + \
+                f"roles={self.roles}, users={self.users}, " + \
+                f"replied_user={self.replied_user})"
+
     class Embed:
         class Footer:
             def __init__(
@@ -82,6 +90,11 @@ class DiscordModel:
                 self.text = text
                 self.icon_url = icon_url
                 self.proxy_icon_url = proxy_icon_url
+
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Footer(text={self.text}, " + \
+                    f"icon_url={self.icon_url}, " + \
+                    f"proxy_icon_url={self.proxy_icon_url})"
 
         class Image:
             def __init__(
@@ -96,6 +109,11 @@ class DiscordModel:
                 self.height = height
                 self.width = width
 
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Image(url={self.url}, " + \
+                    f"proxy_url={self.proxy_url}, height={self.height}, " + \
+                    f"width={self.width})"
+
         class Thumbnail:
             def __init__(
                 self,
@@ -108,6 +126,11 @@ class DiscordModel:
                 self.proxy_url = proxy_url
                 self.height = height
                 self.width = width
+
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Thumbnail(url={self.url}, " + \
+                    f"proxy_url={self.proxy_url}, height={self.height}, " + \
+                    f"width={self.width})"
 
         class Video:
             def __init__(
@@ -122,6 +145,11 @@ class DiscordModel:
                 self.height = height
                 self.width = width
 
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Video(url={self.url}, " + \
+                    f"proxy_url={self.proxy_url}, height={self.height}, " + \
+                    f"width={self.width})"
+
         class Provider:
             def __init__(
                 self,
@@ -130,6 +158,10 @@ class DiscordModel:
             ) -> None:
                 self.name = name
                 self.url = url
+
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Provider(name={self.name}, " + \
+                    f"url={self.url})"
 
         class Author:
             def __init__(
@@ -144,6 +176,11 @@ class DiscordModel:
                 self.icon_url = icon_url
                 self.proxy_icon_url = proxy_icon_url
 
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Author(name={self.name}, " + \
+                    f"url={self.url}, icon_url={self.icon_url}, " + \
+                    f"proxy_icon_url={self.proxy_icon_url})"
+
         class Field:
             def __init__(
                 self,
@@ -154,6 +191,10 @@ class DiscordModel:
                 self.name = name
                 self.value = value
                 self.inline = inline
+
+            def __repr__(self) -> str:
+                return f"DiscordModel.Embed.Field(name={self.name}, " + \
+                    f"value={self.value}, inline={self.inline})"
 
         def __init__(
             self,
@@ -185,6 +226,15 @@ class DiscordModel:
             self.author = author
             self.fields = fields
 
+        def __repr__(self) -> str:
+            return f"DiscordModel.Embed(title={self.title}), " + \
+                f"type={self.type}, description={self.description}, " + \
+                f"url={self.url}, timestamp={self.timestamp}, " + \
+                f"color={self.color}, footer={self.footer}, " + \
+                f"image={self.image}, thumbnail={self.thumbnail}, " + \
+                f"video={self.video}, provider={self.provider}, " + \
+                f"author={self.author}, fields={self.fields})"
+
     class Emoji:
         def __init__(
             self,
@@ -206,6 +256,13 @@ class DiscordModel:
             self.animated = animated
             self.available = available
 
+        def __repr__(self) -> str:
+            return f"DiscordModel.Emoji(id={self.id}), name={self.name}, " + \
+                f"roles={self.roles}, user={self.user}, " + \
+                f"require_colons={self.require_colons}, " + \
+                f"managed={self.managed}, animated={self.animated}, " + \
+                f"available={self.available})"
+
     class MessageReference:
         def __init__(
             self,
@@ -219,6 +276,12 @@ class DiscordModel:
             self.guild_id = guild_id
             self.fail_if_not_exists = fail_if_not_exists
 
+        def __repr__(self) -> str:
+            return "DiscordModel.MessageReference(message_id=" + \
+                f"{self.message_id}, channel_id={self.channel_id}, " + \
+                f"guild_id={self.guild_id}, " + \
+                f"fail_if_not_exists={self.fail_if_not_exists})"
+
     class ActionRowComponent:
         def __init__(
             self,
@@ -230,6 +293,12 @@ class DiscordModel:
         ):
             self.type = DiscordType.Component.ACTION_ROW
             self.components = components
+
+        def __repr__(self) -> str:
+            return "DiscordModel.ActionRowComponent(" + \
+                f"type={self.type}, " + \
+                f"components={self.components}" + \
+                ")"
 
     class ButtonComponent:
         def __init__(
@@ -249,6 +318,17 @@ class DiscordModel:
             self.url = url
             self.disabled = disabled
 
+        def __repr__(self) -> str:
+            return "DiscordModel.ButtonComponent(" + \
+                f"type={self.type}, " + \
+                f"style={self.style}, " + \
+                f"label={self.label}, " + \
+                f"emoji={self.emoji}, " + \
+                f"custom_id={self.custom_id}, " + \
+                f"url={self.url}, " + \
+                f"disabled={self.disabled}" + \
+                ")"
+
     class SelectMenuComponent:
         class Option:
             def __init__(
@@ -264,6 +344,15 @@ class DiscordModel:
                 self.description = description
                 self.emoji = emoji
                 self.default = default
+
+            def __repr__(self) -> str:
+                return "DiscordModel.SelectMenuComponent.Option(" + \
+                    f"label={self.label}, " + \
+                    f"value={self.value}, " + \
+                    f"description={self.description}, " + \
+                    f"emoji={self.emoji}, " + \
+                    f"default={self.default}" + \
+                    ")"
 
         def __init__(
             self,
@@ -281,6 +370,17 @@ class DiscordModel:
             self.min_values = min_values
             self.max_values = max_values
             self.disabled = disabled
+
+        def __repr__(self) -> str:
+            return "DiscordModel.SelectMenuComponent(" + \
+                f"type={self.type}, " + \
+                f"custom_id={self.custom_id}, " + \
+                f"options={self.options}, " + \
+                f"placeholder={self.placeholder}, " + \
+                f"min_values={self.min_values}, " + \
+                f"max_values={self.max_values}, " + \
+                f"disabled={self.disabled}" + \
+                ")"
 
     class TextInputComponent:
         def __init__(
@@ -303,6 +403,19 @@ class DiscordModel:
             self.required = required
             self.value = value
             self.placeholder = placeholder
+
+        def __repr__(self) -> str:
+            return "DiscordModel.TextInputComponent(" + \
+                f"type={self.type}, " + \
+                f"custom_id={self.custom_id}, " + \
+                f"style={self.style}, " + \
+                f"label={self.label}, " + \
+                f"min_length={self.min_length}, " + \
+                f"max_length={self.max_length}, " + \
+                f"required={self.required}, " + \
+                f"value={self.value}, " + \
+                f"placeholder={self.placeholder}" + \
+                ")"
 
     class User:
         def __init__(
@@ -339,3 +452,21 @@ class DiscordModel:
             self.flags = flags
             self.premium_type = premium_type
             self.public_flags = public_flags
+
+        def __repr__(self) -> str:
+            return "DiscordModel.User(" + \
+                f"id={self.id}, " + \
+                f"username={self.username}, " + \
+                f"discriminator={self.discriminator}, " + \
+                f"avatar={self.avatar}, " + \
+                f"bot={self.bot}, " + \
+                f"system={self.system}, " + \
+                f"mfa_enabled={self.mfa_enabled}, " + \
+                f"banner={self.banner}, " + \
+                f"accent_color={self.accent_color}, " + \
+                f"locale={self.locale}, " + \
+                f"verified={self.verified}, " + \
+                f"email={self.email}, " + \
+                f"flags={self.flags}, " + \
+                f"premium_type={self.premium_type}, " + \
+                f"public_flags={self.public_flags})"
